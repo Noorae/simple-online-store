02fdd2de6d10b4806d467093b0605105e0d7618d
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-11-04 23:04:29 +0200

    chore: add bootstrap client

---
+++ storeclient/app/model/Base.js
@@ -0,0 +1,7 @@
+Ext.define('storeClient.model.Base', {
+    extend: 'Ext.data.Model',
+
+    schema: {
+        namespace: 'storeClient.model'
+    }
+});
---
+++ storeclient/app/model/Personnel.js
@@ -0,0 +1,7 @@
+Ext.define('storeClient.model.Personnel', {
+    extend: 'storeClient.model.Base',
+
+    fields: [
+        'name', 'email', 'phone'
+    ]
+});
