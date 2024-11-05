79cf4037935dbf69f4aa3392dff1596d3f905bd1
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-11-05 15:47:02 +0200

    feat: Login page

--- storeclient/app.js
+++ storeclient/app.js
@@ -3,16 +3,16 @@
  * and launch() the Application class.
  */
 Ext.application({
-    extend: 'storeClient.Application',
+    extend: "storeClient.Application",
 
-    name: 'storeClient',
+    name: "storeClient",
 
     requires: [
         // This will automatically load all classes in the storeClient namespace
         // so that application classes do not need to require each other.
-        'storeClient.*'
+        "storeClient.*",
     ],
 
     // The name of the initial view to create.
-    mainView: 'storeClient.view.main.Main'
+    //mainView: "storeClient.view.main.Main",
 });
