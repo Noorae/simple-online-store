273eb50f2283956dd896b977298cb346e3402d0a
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-11-05 20:25:35 +0200

    feat: add endpoints for UserController

--- backend/store/src/main/java/store/controller/UserController.java
+++ backend/store/src/main/java/store/controller/UserController.java
@@ -2,10 +2,7 @@ package store.controller;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import store.model.Product;
 import store.model.User;
 import store.repository.ProductRepository;
@@ -14,6 +11,8 @@ import store.repository.UserRepository;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:1841")
 public class UserController {
 
     private final UserRepository userRepository;
@@ -25,8 +24,17 @@ public class UserController {
 
     @PostConstruct
     public void init() {
-        System.out.println(userRepository.findAll());
 
+    }
+
+    @GetMapping
+    public List<User> getAllItems() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createItem(@RequestBody User user) {
+        return userRepository.save(user);
     }
 
 
