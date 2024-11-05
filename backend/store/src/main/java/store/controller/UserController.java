56d1b032fee9275e832174ab82a0d6311acbe545
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-11-05 21:11:04 +0200

    feat: add login endpoint to UserController

--- backend/store/src/main/java/store/controller/UserController.java
+++ backend/store/src/main/java/store/controller/UserController.java
@@ -2,6 +2,7 @@ package store.controller;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import store.model.Product;
 import store.model.User;
@@ -35,6 +36,17 @@ public class UserController {
     @PostMapping
     public User createItem(@RequestBody User user) {
         return userRepository.save(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody User loginUser) {
+        User user = userRepository.findByEmailAndPassword(loginUser.getEmail(), loginUser.getPassword());
+
+        if (user != null) {
+            return ResponseEntity.ok("Login was successful!");
+        } else {
+            return ResponseEntity.status(401).body("Invalid email or password");
+        }
     }
 
 
