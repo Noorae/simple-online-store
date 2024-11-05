f73dbf720317529b138c3b98a571e65a5f9078b3
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-11-05 20:27:29 +0200

    feat: add getProductByGtin to ProduxtRepository

--- backend/store/src/main/java/store/repository/ProductRepository.java
+++ backend/store/src/main/java/store/repository/ProductRepository.java
@@ -3,4 +3,6 @@ package store.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import store.model.Product;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {}
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Product findByGtinCode(long gtinCode);
+}
