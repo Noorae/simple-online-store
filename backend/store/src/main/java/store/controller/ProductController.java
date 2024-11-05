0fcb2f5f94351b3bdefde76611fc86c2fcd54b95
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-11-05 19:05:43 +0200

    feat: add product and user controllers

--- backend/store/src/main/java/store/controller/ProductController.java
+++ backend/store/src/main/java/store/controller/ProductController.java
@@ -1,4 +1,40 @@
 package store.controller;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import store.model.Product;
+import store.repository.ProductRepository;
+
+import java.util.List;
+
+@RestController
 public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println(productRepository.findAll());
+
+    }
+
+    @GetMapping
+    public List<Product> getAllItems() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping
+    public Product createItem(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
 }
--- backend/store/src/main/java/store/controller/UserController.java
+++ backend/store/src/main/java/store/controller/UserController.java
@@ -1,5 +1,35 @@
 package store.controller;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import store.model.Product;
+import store.model.User;
+import store.repository.ProductRepository;
+import store.repository.UserRepository;
 
+import java.util.List;
+
+@RestController
 public class UserController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println(userRepository.findAll());
+
+    }
+
+
+
+
 }
