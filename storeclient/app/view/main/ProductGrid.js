117da749ce03ca2309c1dac54010209702edbbe2
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-11-06 01:30:59 +0200

    fix: productgrid to match Product model

--- storeclient/app/view/main/ProductGrid.js
+++ storeclient/app/view/main/ProductGrid.js
@@ -2,14 +2,21 @@ Ext.define("storeClient.view.main.ProductGrid", {
     extend: "Ext.grid.Panel",
     xtype: "productgrid",
 
-    title: "Candy Products",
+    title: "Sweets",
     store: {
         type: "product",
     },
 
     columns: [
-        { text: "Name", dataIndex: "name", flex: 1 },
-        { text: "Price", dataIndex: "price", flex: 1 },
-        { text: "Quantity", dataIndex: "quantity", flex: 1 },
+        { text: "Name", dataIndex: "productName", flex: 1 },
+        { text: "Description", dataIndex: "productDescription", flex: 2 },
+        { text: "Weight (g)", dataIndex: "product_weight", flex: 1 },
+        { text: "Calories (kcal)", dataIndex: "productKcal", flex: 1 },
     ],
+
+    listeners: {
+        show: function (grid) {
+            grid.getStore().load();
+        },
+    },
 });
