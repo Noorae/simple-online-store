918cd843e2048e96a80ec9f120978046f5daddc4
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-11-05 19:05:10 +0200

    fix: Product model

--- backend/store/src/main/java/store/model/Product.java
+++ backend/store/src/main/java/store/model/Product.java
@@ -18,10 +18,22 @@ public class Product {
     private String product_description;
     private float product_weight;
     private String product_kcal;
-    private int gtin_code;
+    private Long gtin_code;
 
 
     public Product() {
 
+    }
+
+    public Product(String product_name,
+                   String product_description,
+                   float product_weight,
+                   String product_kcal,
+                   Long gtin_code) {
+        setProduct_name(product_name);
+        setProduct_description(product_description);
+        setProduct_weight(product_weight);
+        setProduct_kcal(product_kcal);
+        setGtin_code(gtin_code);
     }
 }
