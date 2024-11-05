b56757c1bd646be49c3f865308ac9f88704369b2
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-11-05 19:07:00 +0200

    feat: add ProductRepository

---
+++ backend/store/src/main/java/store/repository/ProductRepository.java
@@ -0,0 +1,6 @@
+package store.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import store.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {}
