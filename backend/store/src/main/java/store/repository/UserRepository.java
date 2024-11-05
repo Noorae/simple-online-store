a0a2145466020bbcf8611a9ad3c6087effa13898
Noora Vainionpaa <noora.t.vainionpaa@tuni.fi>
2024-11-06 01:39:45 +0200

    fix: findByEmailAndPassword in UserRepository

--- backend/store/src/main/java/store/repository/UserRepository.java
+++ backend/store/src/main/java/store/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package store.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import store.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmailAndPassword(String email, String password);
+    @Query("SELECT u FROM User u WHERE u.email = :email AND u.password = :password")
+    User findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
 }
